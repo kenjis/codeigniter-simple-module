36afc8f764e116f3bfeb0556e63d71b3b24036df
kenjis <kenji.uui@gmail.com>
2015-10-20 10:45:26 +0900

    Install CodeIgniter 3.0.2

---
+++ bin/my-codeigniter.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd `dirname $0`
+cd ..
+
+# Install translations
+php bin/install.php translations develop
+
+# Install Codeigniter Matches CLI
+php bin/install.php matches-cli master
+
+# Install Cli for CodeIgniter
+composer require kenjis/codeigniter-cli:1.0.x@dev --dev
+php vendor/kenjis/codeigniter-cli/install.php
+
+# Install CI PHPUnit Test
+composer require kenjis/ci-phpunit-test:1.0.x@dev --dev
+php vendor/kenjis/ci-phpunit-test/install.php
+
+# Install CodeIgniter Simple and Secure Twig
+composer require kenjis/codeigniter-ss-twig:1.0.x@dev
+php vendor/kenjis/codeigniter-ss-twig/install.php
+
+# Install CodeIgniter Deployer
+composer require kenjis/codeigniter-deployer:1.0.x@dev --dev
+php vendor/kenjis/codeigniter-deployer/install.php
